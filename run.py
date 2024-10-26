4183b2204974a04fc0aed1bd6756d1fa0f57b78c
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-10-26 18:57:29 +0000

    Welcoming message and some simple rules

---
+++ run.py
@@ -0,0 +1,20 @@
+print(''' 
+    
+Welcome to Tic-Tac-Toe Game! 
+__________________________________
+
+Let the Game Begin!    
+_______________________
+
+Simple Game Rules:
+    Player 1 will be X , and Player 2 will be O.
+    Players take turns putting their marks in empty squares. 
+    The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.
+    When all 9 squares are full, the game is over. 
+    If no player has 3 marks in a row, the game ends in a draw.
+      
+     
+     Enjoy playing!!
+___________________________
+
+''')
