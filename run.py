ab152f158b0f4f6c72a936c004b4fd3126204ab4
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-10-26 19:12:19 +0000

    Function to check if player won

--- run.py
+++ run.py
@@ -42,4 +42,25 @@ def print_board():
 |  {} |  {} |  {} |
 |____|____|____|        
 
-'''.format(*display_positon))  
+'''.format(*display_positon))
+
+# Checks if player has won
+def check_win(num):
+    if board[0] == num and board[1] == num and board[2] == num:
+      return True
+    elif board[3] == num and board[4] == num and board[5] == num:
+      return True
+    elif board[6] == num and board[7] == num and board[8] == num:
+      return True
+    elif board[0] == num and board[3] == num and board[6] == num:
+      return True
+    elif board[1] == num and board[4] == num and board[7] == num:
+      return True
+    elif board[2] == num and board[5] == num and board[8] == num:
+      return True
+    elif board[0] == num and board[4] == num and board[8] == num:
+      return True
+    elif board[2] == num and board[4] == num and board[6] == num:
+      return True
+    else:
+      return False
